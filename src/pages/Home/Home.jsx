79442cc8ee843d12d5398ddc647baa5bf26cdee4
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-04-10 18:14:35 -0700

    enter

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import { Navbar, Brand, Pricing } from '../../components/export';
-import Hero from '../../components/Hero/Hero';
+import Hero from '../../components/hero/Hero';
 import './home.css';
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar />
-      <Brand />
       <Hero/>
+      <Brand />
+      <Hero />
       <Pricing />
     </div>
   )
