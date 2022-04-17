db1aeb486f94a0ed1d6be6653b90c0326390d97b
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-04-16 19:21:21 -0700

    enter

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import { Navbar, Brand, Pricing, Canais, Advantages, Test } from '../../components/export';
 import Hero from '../../components/hero/Hero';
-import './home.css';
 
 const Home = () => {
   return (
