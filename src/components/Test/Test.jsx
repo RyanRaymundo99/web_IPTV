4b2b017efbf72687e73d78a19c102d3be75ce223
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-04-18 16:29:21 -0700

    enter

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -19,7 +19,7 @@ const Test = () => {
                     <label for="email" class="leading-7 text-sm text-gray-400">Email</label>
                     <input type="email" id="email" name="email" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-purple-900 rounded border border-gray-600 focus:border-purple-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                 </div>
-                <button class="text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">Enviar</button>
+                <button class="text-white button__bg border-0 py-2 px-8 focus:outline-none hover:bg-orange-800 rounded text-lg">Enviar</button>
                 <p class="text-xs mt-3">A amostra sera enviada em ate 24h.</p>
                 </div>
             </div>
