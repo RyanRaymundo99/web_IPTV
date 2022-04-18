3cf7c4bf82c09771c41e5cc05709bceead3e05c5
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-04-18 16:51:20 -0700

    enter

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,4 +1,5 @@
 import './hero.css';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 const Hero = () => {
   return (
@@ -11,6 +12,12 @@ const Hero = () => {
             <p>A possibilidade de assistir seu programa de TV em qualquer lugar, e em qualquer dispositivo com acesso a internet computadores, TV smart, TV BOX, ANDROID, IOS, não ficando preso a uma instalação de antena ou cabo. Com a qualidade ainda superior aos sistemas convencionais.</p>
 
             <a className='header_button' href="#canais">Saiba mais</a>
+        </div>
+
+        <div className='zap'>
+          <a href="#" class="float">
+            <WhatsAppIcon style={{ fontSize: 35 }} className='my-float'/>
+          </a>
         </div>
     </div> 
   )
