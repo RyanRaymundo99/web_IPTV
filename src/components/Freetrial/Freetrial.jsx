4403394bf8141edf95625bd8fe2b4f5169a603d4
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-04-19 19:44:09 -0700

    nter

--- src/components/Freetrial/Freetrial.jsx
+++ src/components/Freetrial/Freetrial.jsx
@@ -1,23 +1,13 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import './freetrial.css';
 
+
+
 const Freetrial = () => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_ci7h285', 'template_x91h5i9', form.current, 'h35vyNm11SpNFIkaU')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
 
   return (
-    <form ref={form} onSubmit={sendEmail} className=" freetrial text-gray-400 body-font">
+    <form action="https://formsubmit.co/atendente046353@gmail.com" method='POST' className=" freetrial text-gray-400 body-font">
            <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
                   <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
                   <h1 class="title-font font-medium text-3xl text-white">Faça um teste rápido agora mesmo!</h1>
@@ -27,11 +17,11 @@ const Freetrial = () => {
                   <h2 class="text-white text-lg font-medium title-font mb-5">Insira aqui</h2>
                   <div class="relative mb-4">
                       <label for="full-name" class="leading-7 text-sm text-gray-400">Nome/Apelido</label>
-                      <input type="text" id="name" name="name" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-purple-900 rounded border border-gray-600 focus:border-purple-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                      <input type="text" id="name" name="name" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-purple-900 rounded border border-gray-600 focus:border-purple-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required/>
                   </div>
                   <div class="relative mb-4">
                       <label for="email" class="leading-7 text-sm text-gray-400">Email</label>
-                      <input type="email" id="email" name="email" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-purple-900 rounded border border-gray-600 focus:border-purple-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                      <input type="email" id="email" name="email" class="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-purple-900 rounded border border-gray-600 focus:border-purple-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required/>
                   </div>
                   <input type='submit' class="text-white button__bg border-0 py-2 px-8 focus:outline-none hover:bg-orange-800 rounded text-lg"/>
 
