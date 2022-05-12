4644709e3f0f0c29fa2b9a4dcd5d41c79c74dab6
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-05-11 17:13:58 -0700

    enter

--- src/components/Apps/Apps.jsx
+++ src/components/Apps/Apps.jsx
@@ -4,7 +4,7 @@ const Apps = () => {
   return (
     <div className='apps'>
         <section class="text-gray-400 bg-gray-900 body-font">
-  <div class="container mx-auto flex flex-col px-5 py-5 justify-center items-center">
+  <div class="container mx-auto flex flex-col px-5 pb-2 pt-5 justify-center items-center">
     <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://blueplay.me/wp-content/uploads/2021/02/cel-1433x1536.png"/>
     <div class="w-full md:w-2/3 flex flex-col mb-16 items-center text-center">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Watch Wherever You Want</h1>
